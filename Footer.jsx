77cfeb9f18648f52ef01973ca42f26a53d5d7adb
Iwin Issac <74755442+iwint@users.noreply.github.com>
2021-06-26 17:20:20 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyright by ShapeAi @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript And React.js</h1>
+      <p> A basic Web Dev React.js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
